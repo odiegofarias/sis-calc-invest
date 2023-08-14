aaf143a5929560b164271db322879d6d20fb9ee2
odiegofarias <diego.farias.fernandes@gmail.com>
2023-08-14 20:46:12 -0300

    Criado template e view render add

--- ativo/views.py
+++ ativo/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    
+
 
     context = {
     
     }
     return render(request, 'ativo/index.html', context)
+
+
+def add(request):
+    return render(request, 'ativo/add.html')
